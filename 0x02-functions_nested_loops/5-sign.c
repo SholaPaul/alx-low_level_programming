5190a768657667c5c0e882348f7d3c00c57bd983
SholaPaul <sholapaul14@gmail.com>
2022-10-18 07:59:46 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_sign - a orogram that orints the sign of a number.
+ * @n: character given.
+ * Return: 1 and prints + if n is greater than zero
+ * or 0 and prints 0 if n is zero
+ * and -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
