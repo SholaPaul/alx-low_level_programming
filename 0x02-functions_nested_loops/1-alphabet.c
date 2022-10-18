7d30ae5b6c00720a9b651e795580a86951975847
SholaPaul <sholapaul14@gmail.com>
2022-10-18 04:24:00 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (good)
+ */
+
+void print_alphabet(void)
+{
+
+	char p = ('a'); set autoindent
+
+	while (p <= 'z') 
+	{
+		    _putchar(p);
+		    p++;
+
+	}
+	_putchar('/n');
+
+}
