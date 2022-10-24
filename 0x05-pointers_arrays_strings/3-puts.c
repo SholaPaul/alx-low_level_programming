6edd8602643af3e92f8a6f09ce9d077a068d70a6
SholaPaul <sholapaul14@gmail.com>
2022-10-24 13:36:07 -0700

    task 2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,11 +9,11 @@
 
 void _puts(char *str)
 {
-	int j = 0;
+	int i = 0;
 
-	while (*(str + j) != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(*(str + i++));
+	_putchar(*(str + i++));
 	}
 	_putchar('\n');
 }
